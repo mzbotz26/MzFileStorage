04d5519458d5d3336da1384cd6c346b30a32536d
mzbotz26 <mrsam2395@gmail.com>
2026-08-30 21:42:18 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -382,12 +382,9 @@ async def create_post(client, user_id, messages, cache: dict):
         owner_id = user_id
         file_unique_id = info['file_unique_id']
         
-        # 1. Base64 payload banayein
-        code_string = f"get_{owner_id}_{file_unique_id}"
-        encoded_string = base64.urlsafe_b64encode(code_string.encode("ascii")).decode("ascii").strip("=")
-        
-        # 2. Koyeb Protection Link
-        link = f"{Config.APP_URL.rstrip('/')}/{encoded_string}"
+        # Normal Telegram Bot Link (Click Here ke liye)
+        bot_username = client.me.username
+        link = f"https://t.me/{bot_username}?start=get_{user_id}_{info['file_unique_id']}"
 
         file_size_str = format_bytes(info['file_size'])
         
