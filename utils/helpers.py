ef4f43a9bef9de9572625efc5f7caec2ca60ed77
mzbotz26 <mrsam2395@gmail.com>
2026-09-09 16:20:08 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -14,7 +14,7 @@ from features.shortener import get_shortlink
 
 try:
     from imdb import Cinemagoer
-    ia = Cinemagoer('http')
+    ia = Cinemagoer()
 except Exception:
     try:
         ia = Cinemagoer()
