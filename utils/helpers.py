b5edf9a32a148018be2fc7efdb9341ad023730a1
mzbotz26 <mrsam2395@gmail.com>
2026-09-18 21:29:03 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -314,7 +314,7 @@ async def clean_and_parse_filename(name: str, cache: dict = None):
         'Hindi', 'English', 'Tamil', 'Telugu', 'Kannada', 'Malayalam', 'Punjabi', 'Japanese', 'Korean', 'Marathi',
         'NF', 'AMZN', 'MAX', 'DSNP', 'ZEE5', 'SONY', 'WEB-DL', 'HDRip', 'WEBRip', 'HEVC', 'x265', 'x264', 'AAC',
         '1tamilmv', 'www', 'Join Us', 'Day', 'BBHin', 'JioCinema', 'Hotstar', 'SonyLiv', 'Voot',
-        'DD5', 'DD', 'DDP', 'Atmos', 'Kbps
+        'DD5', 'DD', 'DDP', 'Atmos', 'Kbps'
     ]
     junk_pattern_re = r'\b(' + r'|'.join(junk_words) + r')\b'
     cleaned_title = re.sub(junk_pattern_re, ' ', title_to_clean, flags=re.IGNORECASE)
